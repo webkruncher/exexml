54b970c52fdc0193dbe92cc2417cac51ad7ea7ad
jmt <Jack.Thompson@Swift.com>
2018-11-25 18:06:02 -0500

    BSD License

--- exeyaml.h
+++ exeyaml.h
@@ -1,3 +1,40 @@
+/*
+* Copyright (c) Jack M. Thompson WebKruncher.com
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the WebKruncher nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY Jack M. Thompson ``AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL Jack M. Thompson BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+// JackMThompson.com
+/*
+	ExeYaml Jack M. Thompson WebKruncher.com
+	Original creation: 11/2018
+	exexml is designed to read yaml into a tight knit C++ hierarchy, and write it back out.
+	Every node is based on a generic node, and can be customized.
+	The only dependency is C++ / STL.
+	The utility does not transform or validate.  
+	yamlxml is designed to be fully contained in a single header file that should never exceed 200 lines of code.
+*/
 
 #ifndef BUILDER_YAML_H
 #define BUILDER_YAML_H
